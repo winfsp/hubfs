f6f1574b4d0ef1066df1e3c29e9b8ab719e99841
Bill Zissimopoulos <billziss@navimatics.com>
2021-10-17 13:50:28 +0100

    overlayfs: refactoring

--- src/fs/overlayfs/overlayfs.go
+++ src/fs/overlayfs/overlayfs.go
@@ -20,7 +20,7 @@ import (
 	"github.com/billziss-gh/cgofuse/fuse"
 )
 
-type Overlayfs struct {
+type filesystem struct {
 	topfs   fuse.FileSystemInterface
 	split   func(path string) (string, string)
 	fsnew   func(prefix string) fuse.FileSystemInterface
@@ -29,22 +29,24 @@ type Overlayfs struct {
 	fsmap   map[string]fuse.FileSystemInterface
 }
 
-func NewOverlayfs(
-	topfs fuse.FileSystemInterface,
-	split func(path string) (string, string),
-	fsnew func(prefix string) fuse.FileSystemInterface,
-	caseins bool) *Overlayfs {
+type Config struct {
+	Topfs   fuse.FileSystemInterface
+	Split   func(path string) (string, string)
+	Fsnew   func(prefix string) fuse.FileSystemInterface
+	Caseins bool
+}
 
-	return &Overlayfs{
-		topfs:   topfs,
-		split:   split,
-		fsnew:   fsnew,
-		caseins: caseins,
+func New(c Config) fuse.FileSystemInterface {
+	return &filesystem{
+		topfs:   c.Topfs,
+		split:   c.Split,
+		fsnew:   c.Fsnew,
+		caseins: c.Caseins,
 		fsmap:   make(map[string]fuse.FileSystemInterface),
 	}
 }
 
-func (fs *Overlayfs) destination(path string) (dstfs fuse.FileSystemInterface, remain string) {
+func (fs *filesystem) destination(path string) (dstfs fuse.FileSystemInterface, remain string) {
 	prefix, remain := fs.split(path)
 	if "" == prefix {
 		dstfs = fs.topfs
@@ -66,11 +68,11 @@ func (fs *Overlayfs) destination(path string) (dstfs fuse.FileSystemInterface, r
 	return
 }
 
-func (fs *Overlayfs) Init() {
+func (fs *filesystem) Init() {
 	fs.topfs.Init()
 }
 
-func (fs *Overlayfs) Destroy() {
+func (fs *filesystem) Destroy() {
 	fs.fsmux.Lock()
 	for _, fs := range fs.fsmap {
 		fs.Destroy()
@@ -81,32 +83,32 @@ func (fs *Overlayfs) Destroy() {
 	fs.topfs.Destroy()
 }
 
-func (fs *Overlayfs) Statfs(path string, stat *fuse.Statfs_t) (errc int) {
+func (fs *filesystem) Statfs(path string, stat *fuse.Statfs_t) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Statfs(path, stat)
 }
 
-func (fs *Overlayfs) Mknod(path string, mode uint32, dev uint64) (errc int) {
+func (fs *filesystem) Mknod(path string, mode uint32, dev uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Mknod(path, mode, dev)
 }
 
-func (fs *Overlayfs) Mkdir(path string, mode uint32) (errc int) {
+func (fs *filesystem) Mkdir(path string, mode uint32) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Mkdir(path, mode)
 }
 
-func (fs *Overlayfs) Unlink(path string) (errc int) {
+func (fs *filesystem) Unlink(path string) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Unlink(path)
 }
 
-func (fs *Overlayfs) Rmdir(path string) (errc int) {
+func (fs *filesystem) Rmdir(path string) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Rmdir(path)
 }
 
-func (fs *Overlayfs) Link(oldpath string, newpath string) (errc int) {
+func (fs *filesystem) Link(oldpath string, newpath string) (errc int) {
 	oldprefix, _ := fs.split(oldpath)
 	newprefix, newpath := fs.split(oldpath)
 	if oldprefix != newprefix {
@@ -116,17 +118,17 @@ func (fs *Overlayfs) Link(oldpath string, newpath string) (errc int) {
 	return dstfs.Link(oldpath, newpath)
 }
 
-func (fs *Overlayfs) Symlink(target string, newpath string) (errc int) {
+func (fs *filesystem) Symlink(target string, newpath string) (errc int) {
 	dstfs, newpath := fs.destination(newpath)
 	return dstfs.Symlink(target, newpath)
 }
 
-func (fs *Overlayfs) Readlink(path string) (errc int, target string) {
+func (fs *filesystem) Readlink(path string) (errc int, target string) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Readlink(path)
 }
 
-func (fs *Overlayfs) Rename(oldpath string, newpath string) (errc int) {
+func (fs *filesystem) Rename(oldpath string, newpath string) (errc int) {
 	oldprefix, _ := fs.split(oldpath)
 	newprefix, newpath := fs.split(oldpath)
 	if oldprefix != newprefix {
@@ -136,77 +138,77 @@ func (fs *Overlayfs) Rename(oldpath string, newpath string) (errc int) {
 	return dstfs.Rename(oldpath, newpath)
 }
 
-func (fs *Overlayfs) Chmod(path string, mode uint32) (errc int) {
+func (fs *filesystem) Chmod(path string, mode uint32) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Chmod(path, mode)
 }
 
-func (fs *Overlayfs) Chown(path string, uid uint32, gid uint32) (errc int) {
+func (fs *filesystem) Chown(path string, uid uint32, gid uint32) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Chown(path, uid, gid)
 }
 
-func (fs *Overlayfs) Utimens(path string, tmsp []fuse.Timespec) (errc int) {
+func (fs *filesystem) Utimens(path string, tmsp []fuse.Timespec) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Utimens(path, tmsp)
 }
 
-func (fs *Overlayfs) Access(path string, mask uint32) (errc int) {
+func (fs *filesystem) Access(path string, mask uint32) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Access(path, mask)
 }
 
-func (fs *Overlayfs) Create(path string, flags int, mode uint32) (errc int, fh uint64) {
+func (fs *filesystem) Create(path string, flags int, mode uint32) (errc int, fh uint64) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Create(path, flags, mode)
 }
 
-func (fs *Overlayfs) Open(path string, flags int) (errc int, fh uint64) {
+func (fs *filesystem) Open(path string, flags int) (errc int, fh uint64) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Open(path, flags)
 }
 
-func (fs *Overlayfs) Getattr(path string, stat *fuse.Stat_t, fh uint64) (errc int) {
+func (fs *filesystem) Getattr(path string, stat *fuse.Stat_t, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Getattr(path, stat, fh)
 }
 
-func (fs *Overlayfs) Truncate(path string, size int64, fh uint64) (errc int) {
+func (fs *filesystem) Truncate(path string, size int64, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Truncate(path, size, fh)
 }
 
-func (fs *Overlayfs) Read(path string, buff []byte, ofst int64, fh uint64) (errc int) {
+func (fs *filesystem) Read(path string, buff []byte, ofst int64, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Read(path, buff, ofst, fh)
 }
 
-func (fs *Overlayfs) Write(path string, buff []byte, ofst int64, fh uint64) (errc int) {
+func (fs *filesystem) Write(path string, buff []byte, ofst int64, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Write(path, buff, ofst, fh)
 }
 
-func (fs *Overlayfs) Flush(path string, fh uint64) (errc int) {
+func (fs *filesystem) Flush(path string, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Flush(path, fh)
 }
 
-func (fs *Overlayfs) Release(path string, fh uint64) (errc int) {
+func (fs *filesystem) Release(path string, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Release(path, fh)
 }
 
-func (fs *Overlayfs) Fsync(path string, datasync bool, fh uint64) (errc int) {
+func (fs *filesystem) Fsync(path string, datasync bool, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Fsync(path, datasync, fh)
 }
 
-func (fs *Overlayfs) Opendir(path string) (errc int, fh uint64) {
+func (fs *filesystem) Opendir(path string) (errc int, fh uint64) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Opendir(path)
 }
 
-func (fs *Overlayfs) Readdir(path string,
+func (fs *filesystem) Readdir(path string,
 	fill func(name string, stat *fuse.Stat_t, ofst int64) bool,
 	ofst int64,
 	fh uint64) (errc int) {
@@ -214,37 +216,37 @@ func (fs *Overlayfs) Readdir(path string,
 	return dstfs.Readdir(path, fill, ofst, fh)
 }
 
-func (fs *Overlayfs) Releasedir(path string, fh uint64) (errc int) {
+func (fs *filesystem) Releasedir(path string, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Releasedir(path, fh)
 }
 
-func (fs *Overlayfs) Fsyncdir(path string, datasync bool, fh uint64) (errc int) {
+func (fs *filesystem) Fsyncdir(path string, datasync bool, fh uint64) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Fsyncdir(path, datasync, fh)
 }
 
-func (fs *Overlayfs) Setxattr(path string, name string, value []byte, flags int) (errc int) {
+func (fs *filesystem) Setxattr(path string, name string, value []byte, flags int) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Setxattr(path, name, value, flags)
 }
 
-func (fs *Overlayfs) Getxattr(path string, name string) (errc int, value []byte) {
+func (fs *filesystem) Getxattr(path string, name string) (errc int, value []byte) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Getxattr(path, name)
 }
 
-func (fs *Overlayfs) Removexattr(path string, name string) (errc int) {
+func (fs *filesystem) Removexattr(path string, name string) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Removexattr(path, name)
 }
 
-func (fs *Overlayfs) Listxattr(path string, fill func(name string) bool) (errc int) {
+func (fs *filesystem) Listxattr(path string, fill func(name string) bool) (errc int) {
 	dstfs, path := fs.destination(path)
 	return dstfs.Listxattr(path, fill)
 }
 
-func (fs *Overlayfs) Chflags(path string, flags uint32) (errc int) {
+func (fs *filesystem) Chflags(path string, flags uint32) (errc int) {
 	dstfs, path := fs.destination(path)
 	intf, ok := dstfs.(fuse.FileSystemChflags)
 	if !ok {
@@ -253,7 +255,7 @@ func (fs *Overlayfs) Chflags(path string, flags uint32) (errc int) {
 	return intf.Chflags(path, flags)
 }
 
-func (fs *Overlayfs) Setcrtime(path string, tmsp fuse.Timespec) (errc int) {
+func (fs *filesystem) Setcrtime(path string, tmsp fuse.Timespec) (errc int) {
 	dstfs, path := fs.destination(path)
 	intf, ok := dstfs.(fuse.FileSystemSetcrtime)
 	if !ok {
@@ -262,7 +264,7 @@ func (fs *Overlayfs) Setcrtime(path string, tmsp fuse.Timespec) (errc int) {
 	return intf.Setcrtime(path, tmsp)
 }
 
-func (fs *Overlayfs) Setchgtime(path string, tmsp fuse.Timespec) (errc int) {
+func (fs *filesystem) Setchgtime(path string, tmsp fuse.Timespec) (errc int) {
 	dstfs, path := fs.destination(path)
 	intf, ok := dstfs.(fuse.FileSystemSetchgtime)
 	if !ok {
@@ -271,7 +273,7 @@ func (fs *Overlayfs) Setchgtime(path string, tmsp fuse.Timespec) (errc int) {
 	return intf.Setchgtime(path, tmsp)
 }
 
-var _ fuse.FileSystemInterface = (*Overlayfs)(nil)
-var _ fuse.FileSystemChflags = (*Overlayfs)(nil)
-var _ fuse.FileSystemSetcrtime = (*Overlayfs)(nil)
-var _ fuse.FileSystemSetchgtime = (*Overlayfs)(nil)
+var _ fuse.FileSystemInterface = (*filesystem)(nil)
+var _ fuse.FileSystemChflags = (*filesystem)(nil)
+var _ fuse.FileSystemSetcrtime = (*filesystem)(nil)
+var _ fuse.FileSystemSetchgtime = (*filesystem)(nil)
