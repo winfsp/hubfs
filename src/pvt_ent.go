26f173e246fec4dc985b2bed81f2ba37da9de3e1
Bill Zissimopoulos <billziss@navimatics.com>
2022-05-01 17:52:23 +0100

    main: pvt builds

---
+++ src/pvt_ent.go
@@ -0,0 +1,25 @@
+//go:build ent
+// +build ent
+
+/*
+ * pvt_ent.go
+ *
+ * Copyright 2021-2022 Bill Zissimopoulos
+ */
+/*
+ * This file is part of Hubfs.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License version 3 as published by the Free
+ * Software Foundation.
+ */
+
+package main
+
+import (
+	"github.com/winfsp/hubfs/pvt/prov"
+)
+
+func init() {
+	prov.Load()
+}
